8214f1ced4f825656df3c974988eff8770b8a20c
HassanAhmed25 <hassanahmed2569@gmail.com>
2017-12-12 02:44:29 +0500

    fact

---
+++ fact/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Enter number: ");
+            int fact = Convert.ToInt32(Console.ReadLine());
+            for (int i = fact - 1; i >= 1; i--)
+			{
+                num = fact * i;
+			}
+            Console.WriteLine("Factorial is:    " + num);
+
+            Console.ReadKey();
+        }
+    }
+}
